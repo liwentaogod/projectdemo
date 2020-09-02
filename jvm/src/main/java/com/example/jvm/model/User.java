ae40feb8e7513c4cdae08464e622ab7d02480590
wentaoli <15121182769@163.com>
2020-09-02 16:31:05 +0800

    搭建多模块项目，完成jvm

---
+++ jvm/src/main/java/com/example/jvm/model/User.java
@@ -0,0 +1,36 @@
+package com.example.jvm.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -6622958117423426980L;
+
+    private String name;
+    private int  age;
+    private String telephone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
--- src/test/java/com/wentao/li/LiApplicationTests.java
+++
@@ -1,7 +0,0 @@
-package com.wentao.li;
-
-
-class LiApplicationTests {
-
-
-}
